44ac6eeffd9062883845315cffa5414958544103
Akaya2019 <a.kaya2019@gtu.edu.tr>
2022-05-08 16:11:01 +0300

    ticket reservation methodlarinda siralama yapma eklendi

--- DataProject/src/com/Utilities.java
+++ DataProject/src/com/Utilities.java
@@ -10,11 +10,6 @@ public abstract class Utilities {
     static ArrayList<Tour> tours = new ArrayList<>(); // tours confirmed by admin
     static ArrayList<Hotel> hotels = new ArrayList<>(); // hotels confirmed by admin
 
-    // tour ve hotel requests kalkıcak.
-    // TODO
-    // static ArrayList<Tour> tourrequest = new ArrayList<>(); // tours added by manager but not confirmed yet
-    // static ArrayList<Hotel> hotelrequest = new ArrayList<>(); // hotels added by manager but not confirmed yet
-
     // for tours.
     public void searchTours() {
         int choose;
